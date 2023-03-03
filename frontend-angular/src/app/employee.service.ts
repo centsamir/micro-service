326c2383c4e4f2b8689cab8f6f3fd1a2bae7e637
centsamir <idrissisamir.pro@gmail.com>
2023-03-03 11:37:30 +0100

    regenerate bdd files

--- frontend-angular/src/app/employee.service.ts
+++ frontend-angular/src/app/employee.service.ts
@@ -8,7 +8,7 @@ import { Employee } from './employee';
 })
 export class EmployeeService {
 
-  private baseURL = "http://localhost:8081/api/v1/employees";
+  private baseURL = "http://mygoapp:8081/api/v1/employees";
 
   constructor(private httpClient: HttpClient) { }
   
